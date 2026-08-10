560606a08704d95e5092584e64b62f05abe04d76
Rohit Mathur <127666041+RohitMathur747@users.noreply.github.com>
2026-08-10 23:32:20 +0530

    Day 1 - Set Up Frontend and Banckend Folders and thier dependency is Done!!!

---
+++ Backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { connectDB } from "./config/db.js";
+
+const PORT = 5000;
+const app = express();
+
+//db
+connectDB();
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+//routes
+app.get("/", (req, res) => {
+  res.send("API working");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Started on http://localhost:${PORT}`);
+});

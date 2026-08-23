2da98f58e75f86e492e1c6021441041916ab603b
Rohit Mathur <127666041+RohitMathur747@users.noreply.github.com>
2026-08-23 15:17:04 +0530

    Day 8 - admin setup with navbar completed

---
+++ Admin/src/App.jsx
@@ -0,0 +1,18 @@
+//import React from "react";
+
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
